4b94ecbd7ac78c02e0e758775915608d9835d89e
konradgKainos <kglowczynski@users.noreply.github.com>
2024-08-16 13:28:29 +0200

    Update App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Update 2
+          Update 21
         </p>
         <a
           className="App-link"
