dd4344ee0d923e7049964e4119ba5c40b3941a7e
konradgKainos <kglowczynski@users.noreply.github.com>
2024-08-19 16:48:57 +0200

    check aws lambda function invocation

--- my-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ my-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,11 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "getContentFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const fetchData = async () => {
+  try {
+    const response = await fetch('https://1sv2k4phta.execute-api.eu-central-1.amazonaws.com/dev', {
+      method: 'GET', // or 'POST', 'PUT', etc.
+      headers: {
+        'Content-Type': 'application/json'      },
+      // body: JSON.stringify(yourData) // for POST or PUT requests
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+fetchData()
 
 function App() {
   return (
@@ -8,7 +30,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Update 212fsdg
+          fetchData
         </p>
         <a
           className="App-link"
